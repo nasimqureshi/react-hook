067a39b0e537b842571d7b723e390436af536cfc
nasimqureshi <qureshi.nasim@yahoo.com>
2021-01-31 13:43:51 -0600

    first commit

---
+++ hook/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+let count = 1;
+
+const IncNum = () => {
+    count++;
+    console.log('clicked');
+}
+
+const App = () => {
+    return (
+        <>
+        <h1>{count}</h1>
+        <button onClick={IncNum}>Click Me</button>
+        </>
+    )
+
+}
+
+export default App;
